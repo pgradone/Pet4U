1b285289d08a3f40eab54e3ab0d7016d6bf85ff1
unknown <pgradone@gmail.com>
2020-06-12 15:12:01 +0200

    created loop to add cards to the article and <option>s of the select

---
+++ scripts/script.js
@@ -0,0 +1,35 @@
+// -- work with animalsArray
+
+// generate a “card columns” layout <-- refer to <figure> tag inside the #animals
+// Ref: code adapted from my Actors Agency project
+// Loop through Animals List
+// define parent cards container of Animals
+const cardGrid = document.querySelector("#animals");
+// define mock Card to clone
+const mockCard = cardGrid.querySelector("figure");
+// define the animal's select block where to append the animals' options
+const animalSelect = document.querySelector("#select-animal");
+// the element to duplicate is the first option of the select.
+const mockAnimalOption = animalSelect.querySelector("option");
+// populate the generated cards with the picture and the name of the animal.
+animalsArray.forEach((animal) => {
+  // Create a variable "newCard" and append it to the #animals <article>
+  const newCard = mockCard.cloneNode(true);
+  cardGrid.append(newCard);
+  // In this variable, find the <figcaption> and change the text of the html element with the property value "actor.name"
+  newCard.querySelector("figcaption").textContent = animal.name;
+  // In this newCard, find the img and change its "src" attribute value.
+  newCard.querySelector("img").src = animal.picture;
+  // Add the animal "type" as a class : this step may be useful for the filtering later on.
+  newCard.classList.add(animal.type);
+  // 6. Remove the class "d-none" to make this duplicated card visible.
+  //   newCard.classList.remove("d-none");
+  // clone the animal option and append it to the <select>
+  const newAnimalOption = mockAnimalOption.cloneNode(true);
+  animalSelect.append(newAnimalOption);
+  newAnimalOption.value = animal.name;
+  newAnimalOption.innerHTML = animal.name;
+  newAnimalOption.removeAttribute("id");
+});
+mockCard.remove();
+mockAnimalOption.remove;
